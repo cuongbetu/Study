f83f4811de93dca8dfac2276ec84aed6b0cbcb1b
Cuong Nguyen <cuongbet@gmail.com>
2020-07-01 22:41:04 +0700

    Form

--- src/App.js
+++ src/App.js
@@ -1,54 +1,133 @@
 import React, { Component } from 'react';
-import ColorPicker from './components/ColorPicker';
-import SizeSetting from './components/SizeSetting';
-import Reset from './components/Reset';
-import Result from './components/Result';
 import './App.css';
 class App extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
     this.state = {
-      color : 'red',
-      fontSize : 16,
+      txtUsername : '',
+      txtPassword : '',
+      txtDescription :'',
+      sltGender : 0,
+      rdLang : 'vi',
+      chkbStatus : false
     }
-  };
-
-  onSetColor = (params) =>{
+  }
+  onHandleChange = (event) => {
+    let target = event.target;
+    let name = target.name;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      color : params
+      [name] : value
     });
   }
 
-  onSetFontSize = (params) => {
-    if (this.state.fontSize + params >= 16 && this.state.fontSize + params <= 50) {
-      this.setState({
-         fontSize : this.state.fontSize +  params
-      });
-    }
-  }
-
-  onReset = (value) =>{
-    if (value) {
-      this.setState({
-        color : 'red',
-        fontSize : 16
-      });
-    }
+  onHandleSubmit = (event) =>{
+    event.preventDefault();
+    console.log(this.state)
   }
 
   render(){
-    return (
+   
+
+    return (   
       <div className="container mt-50">
-        <div className="row">
-          <ColorPicker color = { this.state.color } onReceiveColor = { this.onSetColor }></ColorPicker>
-          <SizeSetting fontSize= {this.state.fontSize} onReceiveFontSize = { this.onSetFontSize } ></SizeSetting>
-        </div>
-        <div className="row">
-            <Result color={this.state.color} fontSize = {this.state.fontSize}></Result>
-            <Reset onReceiveReset = {this.onReset }></Reset>
-          </div> 
-        </div>
+          <div className="row">
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Form</h3>
+                </div>
+                <div className="panel-body">
+                  <form onSubmit = { this.onHandleSubmit }>
+                    <div className="form-group">
+                      <label>Username :</label>
+                      <input type="text"
+                             className="form-control" 
+                             name="txtUsername" 
+                             placeholder="Xin mời nhập tên tài khoản"
+                             onChange = { this.onHandleChange}  
+                             />
+                      <label>Password :</label>
+                      <input type="password"
+                             className="form-control" 
+                             name="txtPassword" 
+                             placeholder="Xin mời nhập mật khẩu"
+                             onChange = { this.onHandleChange}  
+                             />
+
+                      <label>Mô tả :</label>
+                      <textarea 
+                             className="form-control" 
+                             name="txtDescription" 
+                             placeholder="Xin mời nhập mô tả"
+                             onChange = { this.onHandleChange}  
+                             />
+
+                      <label>Giới tính :</label>
+                      <select 
+                             className="form-control" 
+                             name="sltGender" 
+                             onChange = { this.onHandleChange}
+                             value =  {this.state.sltGender}
+                            >
+                            <option value= {0}>
+                              Nữ
+                            </option>
+                            <option value= {1}>
+                              Nam
+                            </option>
+                      </select>
+                      <label>
+                        Ngôn ngữ
+                      </label>
+                      <div className="radio">
+                        <label>
+                          <input 
+                                value="vi"
+                                name="rdLang"
+                                type="radio"
+                                onChange = { this.onHandleChange}
+                                checked = { this.state.rdLang === "vi"}
+                                />
+                                Tiếng Việt
+                        </label>
+                        <br/>
+                        <label>
+                          <input 
+                                value="en"
+                                name="rdLang"
+                                type="radio"
+                                onChange = { this.onHandleChange}
+                                checked = { this.state.rdLang === "en"}
+                                />
+                                Tiếng Anh
+                        </label>
+                      </div>
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox"
+                                 value= {true}
+                                 name="chkbStatus"  
+                                 onChange = { this.onHandleChange }
+                                 checked= { this.state.chkbStatus === true}
+                                 />
+                          Trạng thái
+                        </label>
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Lưu lại</button>&nbsp;
+                    <button type="reset"  className="btn btn-info">Xóa form</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+ 
+  
+    
+    
+    
     );
   }
   
